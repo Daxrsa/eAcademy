cf8333c4837bb19eeb55ab25fba2b52d4ca9f67c
Daorsa Hyseni <79546481+Daxrsa@users.noreply.github.com>
2022-02-26 23:25:26 +0100

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,69 @@
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<div class="extra">
+      
+      <ul class="lista">
+        
+        <p>FOLLOW US</p>
+
+        <i class="fab fa-twitter"></i>
+        <li><a href="#">TWITTER</a></li><br>
+        <i class="fab fa-facebook"></i>
+        <li><a href="#">FACEBOOK</a></li><br>
+        <i class="fab fa-instagram"></i>
+        <li><a href="#">INSTAGRAM</a></li><br>
+        <i class="fab fa-github"></i>
+        <li><a href="#">GITHUB</a></li><br>
+        <i class="fab fa-linkedin-in"></i>
+        <li><a href="#">LINKEDIN</a></li><br>
+        <i class="fab fa-youtube"></i>
+        <li><a href="#">YOUTUBE</a></li><br>
+        <i class="fas fa-certificate"></i>
+        <input type="text" name="" placeholder="Certificate ID">
+        <button>Verify</button>
+      </ul>
+      <ul class="lista">
+        <P>STAY CONNECTED</P>
+        <li><a href="#">14121 NE Airport Way, Portland, OR US</a></li><br>
+        <li><a href="#">+1 503 3343514</a></li><br>
+        <li><a href="#">support@eacademy.net</a></li><br>
+      </ul>
+      
+      <ul class="lista">
+        <p>WORKING HOURS</p>
+<li>Monday:     9:30 am - 6.00 pm</li><br>
+<li>Tuesday:    9:30 am - 6.00 pm</li><br>
+<li>Wednesday:  9:30 am - 6.00 pm</li><br>
+<li>Thurday:    9:30 am - 6.00 pm</li><br>
+<li>Friday:     9:30 am - 3.00 pm</li><br>
+<li>Saturday:   <strong>Closed</strong></li><br>
+<li>Sunday:     <strong>Closed</strong></li>
+      </ul>
+    
+</div>
+   <div class="sponsors">
+     <p>Top companies that choose eAcademy Business to build in-demand career skills</p>
+     <div class="a">
+     <img src="images/netflix.png" width="200" height="150">
+     <img src="images/harvard.jpg" width="220" height="150">
+     <img src="images/khan.jpg.opdownload" width="300" height="150">
+     <img src="images/cos.svg" width="220" height="150">
+     <img src="images/coursera.png" width="220" height="150">
+     </div>
+   </div>
+
+   <ul class="liste">
+        <p>eAcademy</p>
+        <li><a href="#">Affiliates</a></li>
+        <li><a href="#">Terms</a></li>
+        <li><a href="#">Privacy Policy</a></li>
+        <li><a href="#">About Us</a></li>
+        <li><a href="#">Certification</a></li>
+        <li><a href="#">Blog</a></li>
+        <li><a href="#">Contact</a></li>
+        <li><a href="#">Teach</a></li>
+        <li><a href="#">Reviews</a></li>
+        <li><a href="#">Refund Policy</a></li>
+      </ul>
+    
+  </body>
+</html>
